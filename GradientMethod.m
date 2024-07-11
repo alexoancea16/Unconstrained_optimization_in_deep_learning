20af20f73c80d4d29ca0c21a44ed42efe83dc39c
Alexandru Oancea <alexandruoancea49@gmail.com>
2024-07-11 16:05:57 +0300

    add numerical results and diagram

--- GradientMethod.m
+++ GradientMethod.m
@@ -1,16 +1,20 @@
 % Gradient Method 
-function [X, x, k] = GradientMethod(A, e, X, x)
+function [X, x, k, X_evolution, x_evolution, time] = GradientMethod(A, e, X, x)
 
 k = 0;
+k_max = 10000;
+X_evolution = zeros(k_max, 1);  
+x_evolution = zeros(k_max, 1);
+time = zeros(k_max, 1);
 alpha = 0.001;
-k_max = 1000;
-eps = 0.01;
-N = size(A,1);
+eps = 0.0001;
+N = size(A, 1);
 M = g(A*X)*x;    % Predictions
 err = M - e;
 grd_x = (1/N) * (g(A*X))' * err;               % First order derivative of the Loss function for x
 grd_X = (1/N) * (g_der(A*X))' * (err * x');    % First order derivative of the Loss function for X
-while norm(grd_X) > eps || norm(grd_x) > eps || k < k_max
+tic
+while norm(grd_X) > eps && norm(grd_x) > eps && k < k_max
     X = X - alpha * grd_X;
     x = x - alpha * grd_x;
     M = g(A*X)*x;    
@@ -18,6 +22,9 @@ while norm(grd_X) > eps || norm(grd_x) > eps || k < k_max
     grd_x = (1/N) * (g(A*X))' * err;              
     grd_X = (1/N) * (g_der(A*X))' * (err * x');   
     k = k + 1; 
+    X_evolution(k) = norm(grd_X);
+    x_evolution(k) = norm(grd_x);
+    time(k) = toc;
 end
 
 end
--- Main.m
+++ Main.m
@@ -12,6 +12,7 @@ A(1:160,12) = 1;      % Training matrix
 T(1:40,12) = 1;       % Test matrix
 X = rand(m,m);        % Network parameters
 x = rand(m,1);
+
 %% Data normalization
 for i = 1:n
     A(:,i) = normalization(A(:,i),N);
@@ -23,4 +24,31 @@ e_test = normalization(e_test,Nt);
 %% Numerical optimization methods
 
 % Gradient Method 
-[X_calculat, x_calculat, iteration] = GradientMethod(A, e, X, x);
+[X_GM, x_GM, iteration_GM, X_evolution_GM, x_evolution_GM, time_GM] = GradientMethod(A, e, X, x);
+
+%% Comparison of results for the use of optimization methods
+
+% Gradient Method
+figGM_x = semilogy(1:iteration_GM-1,x_evolution_GM(1:iteration_GM-1,1));
+figGM_x.LineWidth = 1;
+figGM_x.Color = 'r';
+title('Gradient Method - The evolution of the x-dependent criterion according to the iterations', 'FontSize', 10, 'FontWeight', 'bold');
+xlabel("Number of iterations");
+ylabel("The gradient norm in x");
+grid on;
+figure();
+figGM_X = semilogy(1:iteration_GM-1,X_evolution_GM(1:iteration_GM-1,1));
+figGM_X.LineWidth = 1;
+figGM_X.Color = 'b';
+title('Gradient Method - The evolution of the X-dependent criterion according to the iterations', 'FontSize', 10, 'FontWeight', 'bold');
+xlabel("Number of iterations");
+ylabel("The gradient norm in X");
+grid on;
+figure();
+figGM_t = semilogy(1:iteration_GM-1,time_GM(1:iteration_GM-1,1));
+figGM_t.LineWidth = 1;
+figGM_t.Color = 'g';
+title('Gradient Method - The evolution of the time according to the iterations', 'FontSize', 10, 'FontWeight', 'bold');
+xlabel("Time evolution");
+ylabel("The gradient norm in X");
+grid on;
